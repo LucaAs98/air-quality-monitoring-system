8910a88ae87abba79021bcdd3b38018f7faf3a6e
Luca Asunis <32647882+LucaAs98@users.noreply.github.com>
2022-06-04 16:38:42 +0200

    Altri cambiamenti al menù, apertura di un link esterno al click del bottone per grafana e creato il form per cambiare i parametri dell'esp

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
+//Parametri da cambiare tramite interfaccia:
+let MAX_GAS_VALUE;
+let MIN_GAS_VALUE;
+let SAMPLE_FREQUENCY;
+let PROTOCOL;
+
+
 //Array dei dispositivi da visualizzare. Si dovranno prendere da firebase.
 let arrayESP32 = [
     {
@@ -5,7 +12,7 @@ let arrayESP32 = [
         lat: 44.49712,
         long: 11.34248
     },
-    {
+   {
         id: 'esp1',
         lat: 43.49712,
         long: 10.34248
@@ -124,7 +131,7 @@ function aggiungiCarta(result, idx) {
                         <div class="d-grid gap-2">
                             <button class="btn btn-primary" type="button" onclick=visualizeGrafanaGraphs(${idx})>Vedi su grafana</button>
                             <button class="btn btn-primary" type="button" data-bs-toggle="collapse" href="#map_div_${idx}" role="button" aria-expanded="false" aria-controls="map_div_${idx}">Vedi su mappa</button>
-                            <button class="btn btn-primary" type="button" onclick=changeParameters(${idx})>Cambia parametri</button>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target=#modal${idx} data-bs-whatever=${idx}>Cambia Parametri</button>
                             <button class="btn btn-danger" type="button" onclick=removeEsp32(${idx})>Rimuovi</button>
                         </div>
                     </div>
@@ -133,6 +140,37 @@ function aggiungiCarta(result, idx) {
             <div class="collapse" id="map_div_${idx}">
                 <div id="map${idx}"></div>
             </div>
+        </div>
+       
+        <div class="modal fade" id=modal${idx} tabindex="-1" aria-labelledby=modal${idx}Label aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id=modal${idx}Label>esp${idx}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form>
+                   <div class="mb-3">
+                    <label for="max_gas_value" class="form-label">MAX_GAS_VALUE</label>
+                    <input type="text" class="form-control" id="max_gas_value" placeholder="Metti qui la soglia massima del gas" aria-describedby="max_gas_help">
+                  </div>
+                  <div class="mb-3">
+                    <label for="min_gas_value" class="form-label">MIN_GAS_VALUE</label>
+                    <input type="text" class="form-control" id="min_gas_value" placeholder="Metti qui la soglia minima del gas" aria-describedby="min_gas_help">
+                  </div>
+                  <div class="mb-3">
+                    <label for="sample_frequency_value" class="form-label">SAMPLE FREQUENCY</label>
+                    <input type="text" class="form-control" id="sample_frequency_value" placeholder="Metti qui la sample frequency" aria-describedby="sample_frequency_help">
+                  </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-success" id="change_parameters">Cambia parametri</button>
+              </div>
+            </div>
+          </div>
         </div>
     </div>`
 
@@ -158,19 +196,49 @@ function aggiungiCarta(result, idx) {
         map.invalidateSize();
     });
     resizeObserver.observe($(`#map_div_${idx}`)[0])
+
+    //Comportamento del modal
+    let modal = document.getElementById(`modal${idx}`);
+    modal.addEventListener('show.bs.modal', function (event) {
+        // Bottone che triggera il modal
+        let button = event.relatedTarget;
+        // Prendiamo l'informazione passata
+        let idEsp = button.getAttribute('data-bs-whatever');
+
+        // Aggiorniamo il contenuto del modal
+        let modalTitle = modal.querySelector('.modal-title');
+        let modalBodyInputMax = modal.querySelector('.modal-body input#max_gas_value');
+        let modalBodyInputMin = modal.querySelector('.modal-body input#min_gas_value');
+        let modalBodyInputSample = modal.querySelector('.modal-body input#sample_frequency_value');
+
+        modalTitle.textContent = 'Parametri esp' + idEsp
+
+        /**** Bisogna prendere le informazioni dal db del device con tale id. ****/
+        modalBodyInputMax.value = idEsp
+        modalBodyInputMin.value = idEsp
+        modalBodyInputSample.value = idEsp
+
+        /*** Mandare queste NUOVE infrmazioni al db ***/
+        let modalChangeButton = modal.querySelector('#change_parameters');
+        modalChangeButton.addEventListener('click', function (event){
+            alert("MAX: " + modalBodyInputMax.value + "\nMIN: " + modalBodyInputMin.value + "\nSAMPLE_FREQUENCY: " + modalBodyInputSample.value)
+        })
+
+    })
 }
 
 
 //Funzione chiamata al click del bottone "Vedi su Grafana"
 function visualizeGrafanaGraphs(idx){
-    alert("Visualizza grafico per -> esp" + idx)
+    window.open("https://cyberdude.it/wp-content/uploads/2021/01/science-of-earworms-explain-why-never-gonna-give-you-up-is-stuck-in-our-heads-30-years-later.png", '_blank').focus();
     //Visualizza grafico per esp + idx, potremmo farlo visualizzare sotto, oppure restituire solo il link
 }
 
 //Funzione chiamata al click del bottone "Cambia Parametri"
-function changeParameters(idx){
+function sendNewParameters(idx){
     alert("Cambia parametri per -> esp" + idx)
     //Cambia parametri per + idx
+
 }
 
 //Funzione chiamata al click del bottone "Rimuovi"
